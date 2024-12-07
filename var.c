bd797e4388a3635feb73acc28df1f45be79c8fea
shuvarajRegmi <shuvarajregmi10@gmail.com>
2024-12-07 08:49:46 -0800

    Add files via upload

---
+++ var.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    // Create variables
+    int num = 12; // Integer 
+    float fnum = 59.60; //Floating point number
+    char letter = 'b'; // Character
+    
+    // print variables
+    printf("%d\n",num);
+    printf("%f\n",fnum);
+    printf("%c\n",letter);
+    return 0;
+}
