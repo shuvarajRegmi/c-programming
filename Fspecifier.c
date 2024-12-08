b33e7ea3ba3433fda4ee9d8ff512d2f6e2f3b6cd
shuvarajRegmi <shuvarajregmi10@gmail.com>
2024-12-08 05:46:32 -0800

    Add files via upload

---
+++ Fspecifier.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // create a format
+    int mynum=55;
+    float myfvalue=55.5;
+    char myletter='a';
+    //print the above data
+    printf("My number is:%d\nMy Floate is:%f\nMy letter:%c\n",mynum,myfvalue,myletter);
+    return 0;
+}
